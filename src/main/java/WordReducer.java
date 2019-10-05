2c871315db2a7884d4ed97b63abf728e351d1a07
aleksma <aleksma99@yandex.ru>
2019-10-05 12:16:50 +0300

    [UPD] regex

--- src/main/java/WordReducer.java
+++ src/main/java/WordReducer.java
@@ -11,10 +11,12 @@ public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> 
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         long count = 0;
         Iterator iter = values.iterator();
+
         while (iter.hasNext()) {
             iter.next();
             count++;
         }
+
         context.write(key, new LongWritable(count));
     }
 }
